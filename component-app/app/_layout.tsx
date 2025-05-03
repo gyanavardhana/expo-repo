9b615ca82bbc392c9485b78587e4867eed95f49d
gyanavardhana <gyanavardhanmamidisetti@gmail.com>
2025-05-03 16:42:40 +0530

    three done

--- component-app/app/_layout.tsx
+++ component-app/app/_layout.tsx
@@ -1,9 +1,24 @@
-import { View } from 'react-native';
+import { useState } from 'react';
+import { View, Button } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import ReceiptTower from '@/components/example-components/ReceiptTower';
 import { FinanceTracker } from '@/components/example-components/FinancialTracker';
 import { FinancialGoalsComponent } from '@/components/example-components/FinancialGoalsComponent';
+
 export default function RootLayout() {
+  const [componentIndex, setComponentIndex] = useState(0);
+
+  const components = [
+    <FinanceTracker />,
+    <ReceiptTower selectedYearIndex={0} />,
+    <FinancialGoalsComponent />,
+    
+  ];
+
+  const handleNext = () => {
+    setComponentIndex((prevIndex) => (prevIndex + 1) % components.length);
+  };
+
   return (
     <LinearGradient
       colors={['#6E38BF', '#9239AE', '#863FAF']}
@@ -11,9 +26,8 @@ export default function RootLayout() {
       style={{ flex: 1 }}
     >
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-       { /* <ReceiptTower selectedYearIndex={0} /> */}
-       { /*   <FinanceTracker /> */}
-       <FinancialGoalsComponent />
+        <Button title="Next" onPress={handleNext} />
+        {components[componentIndex]}
       </View>
     </LinearGradient>
   );
